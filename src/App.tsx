72ca37ccf9562ae7825b9ce9c43306aacd9bce6c
Akash Patel <patelakash1627@gmail.com>
2025-05-01 14:30:05 +0530

    fix ci.yml file version issue

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-    
+      <h1>Hello World</h1>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
